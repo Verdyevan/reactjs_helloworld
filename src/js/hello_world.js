3f3d1eb973f528557db28ebb7914a0300368d082
Evan <evantyoverdy95@gmail.com>
2019-07-05 16:31:00 +0700

    hello world in reactjs

---
+++ src/js/hello_world.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class HelloWorld extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2> Changing to new code </h2>
+            </div>
+        )
+    }
+}
